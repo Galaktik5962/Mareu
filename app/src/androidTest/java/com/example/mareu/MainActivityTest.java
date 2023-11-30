404ce45197a52226144bd6ee6ec4cbfeb6749f8e
Émilie P <emiliep.dev@gmail.com>
2023-11-30 11:11:38 +0100

    Simplified instrumented tests + added dependancies in gradle + fixed some xml issues in item_meeting_list

--- app/src/androidTest/java/com/example/mareu/MainActivityTest.java
+++ app/src/androidTest/java/com/example/mareu/MainActivityTest.java
@@ -1,49 +1,33 @@
 package com.example.mareu;
 
 
-import static androidx.appcompat.widget.TintContextWrapper.wrap;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withParent;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.is;
 
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.DataInteraction;
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.IdlingRegistry;
-import androidx.test.espresso.IdlingResource;
 import androidx.test.espresso.NoMatchingViewException;
 import androidx.test.espresso.ViewAssertion;
-import androidx.test.espresso.ViewInteraction;
-import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.fragment.app.testing.FragmentScenario;
-
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
-
-import com.example.mareu.ui.MeetingListFragment;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -58,149 +42,42 @@ public class MainActivityTest {
 
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
-            new ActivityScenarioRule<>(MainActivity.class);
+        new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void mainActivityTest() throws InterruptedException {
-        ViewInteraction floatingActionButton = onView(
-                allOf(withId(R.id.add_button),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        floatingActionButton.perform(click());
+    public void addMeetingTest() {
+        onView(withId(R.id.add_button)).perform(click());
 
-        ViewInteraction button = onView(
-                allOf(withId(R.id.button_add_meeting),
-                        withParent(withParent(withId(R.id.fragment_container))),
-                        isDisplayed()));
-        button.check(matches(isDisplayed()));
+        onView(withId(R.id.button_add_meeting)).check(matches(isDisplayed()));
 
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.sujet_de_la_reunion),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.fragment_container),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatEditText.perform(replaceText("Test"), closeSoftKeyboard());
+        onView(withId(R.id.sujet_de_la_reunion)).perform(replaceText("Test"), closeSoftKeyboard());
 
-        ViewInteraction appCompatImageButton = onView(
-                allOf(withId(R.id.button_calendar),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.fragment_container),
-                                        0),
-                                5),
-                        isDisplayed()));
-        appCompatImageButton.perform(click());
+        // set date to 12/05/2024
+        onView(withId(R.id.button_calendar)).perform(click());
+        onView(withClassName(equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2024, 5, 12));
+        onView(withId(android.R.id.button1)).perform(click());
 
-        ViewInteraction materialButton = onView(
-                allOf(withId(android.R.id.button1), withText("OK"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.ScrollView")),
-                                        0),
-                                3)));
-        materialButton.perform(scrollTo(), click());
+        // set time to 12:30
+        onView(withId(R.id.button_time)).perform(click());
+        onView(withClassName(equalTo(TimePicker.class.getName()))).perform(PickerActions.setTime(12, 30));
+        onView(withId(android.R.id.button1)).perform(click());
 
-        ViewInteraction appCompatImageButton2 = onView(
-                allOf(withId(R.id.button_time),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.fragment_container),
-                                        0),
-                                6),
-                        isDisplayed()));
-        appCompatImageButton2.perform(click());
+        // picking 3rd item in spinner
+        onView(withId(R.id.lieu_de_la_reunion)).perform(click());
+        onData(anything()).atPosition(2).perform(click());
 
-        ViewInteraction materialButton2 = onView(
-                allOf(withId(android.R.id.button1), withText("OK"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.ScrollView")),
-                                        0),
-                                3)));
-        materialButton2.perform(scrollTo(), click());
+        onView(withId(R.id.button_add_mail)).perform(click());
+        // enter email address in opened dialog, EditText doesn't have an id so we match it by class name
+        onView(withClassName(equalTo(android.widget.EditText.class.getName()))).perform(replaceText("test@test.com"), closeSoftKeyboard());
+        onView(withId(android.R.id.button1)).perform(click());
 
-        ViewInteraction appCompatSpinner = onView(
-                allOf(withId(R.id.lieu_de_la_reunion),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.fragment_container),
-                                        0),
-                                1),
-                        isDisplayed()));
-        appCompatSpinner.perform(click());
-
-        DataInteraction appCompatCheckedTextView = onData(anything())
-                .inAdapterView(childAtPosition(
-                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
-                        0))
-                .atPosition(4);
-        appCompatCheckedTextView.perform(click());
-
-        ViewInteraction appCompatImageButton3 = onView(
-                allOf(withId(R.id.button_add_mail), withContentDescription("ajoutez une adresse mail"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.fragment_container),
-                                        0),
-                                7),
-                        isDisplayed()));
-        appCompatImageButton3.perform(click());
-
-        ViewInteraction editText = onView(
-                allOf(childAtPosition(
-                                allOf(withId(android.R.id.custom),
-                                        childAtPosition(
-                                                withClassName(is("android.widget.FrameLayout")),
-                                                0)),
-                                0),
-                        isDisplayed()));
-        editText.perform(replaceText("test@lamzone.com"), closeSoftKeyboard());
-
-        ViewInteraction materialButton3 = onView(
-                allOf(withId(android.R.id.button1), withText("Ajouter"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.ScrollView")),
-                                        0),
-                                3)));
-        materialButton3.perform(scrollTo(), click());
-
-        ViewInteraction materialButton4 = onView(
-                allOf(withId(R.id.button_add_meeting),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.fragment_container),
-                                        0),
-                                11),
-                        isDisplayed()));
-        materialButton4.perform(click());
-
-        // Vérification que le fragment actif est le MeetingListFragment
-        onView(withId(R.id.fragment_container)).check(matches(isDisplayed()));
-
-        // Vérification que la RecyclerView est affichée
-
-        ViewInteraction viewGroup = onView(
-                allOf(withParent(allOf(withId(R.id.recycler_view),
-                                withParent(withId(R.id.fragment_container)))),
-                        isDisplayed()));
-        viewGroup.check(matches(isDisplayed()));
-
-
-        // Vérification du nombre d'éléments dans la RecyclerView du fragment
-        //onView(withId(R.id.recycler_view)).check(new RecyclerViewItemCountAssertion(11));
-
+        onView(withId(R.id.button_add_meeting)).perform(click());
     }
 
     private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
+        final Matcher<View> parentMatcher,
+        final int position
+    ) {
 
         return new TypeSafeMatcher<View>() {
             @Override
@@ -213,7 +90,7 @@ public class MainActivityTest {
             public boolean matchesSafely(View view) {
                 ViewParent parent = view.getParent();
                 return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
+                    && view.equals(((ViewGroup) parent).getChildAt(position));
             }
         };
     }
@@ -227,7 +104,10 @@ class RecyclerViewItemCountAssertion implements ViewAssertion {
     }
 
     @Override
-    public void check(View view, NoMatchingViewException noViewFoundException) {
+    public void check(
+        View view,
+        NoMatchingViewException noViewFoundException
+    ) {
         if (noViewFoundException != null) {
             throw noViewFoundException;
         }
